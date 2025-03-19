cad57aad18f5b62cb6d54361394da1a39d58d7c8
Himanshusharma2046 <himanshusharma4041@gmail.com>
2025-03-19 09:49:07 +0530

    company model , user model , job model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+
 
 
 
@@ -15,8 +20,9 @@ const corsOptions={
     credentials:true
 }
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 app.listen(PORT ,()=>{
+    connectDB();
     console.log(`Server is running at Port ${PORT}`);
 })
